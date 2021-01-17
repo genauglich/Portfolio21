06837dbf654cc016f1d8bd6c18a4a4fd8b519037
Nico <nicostaehli@bluewin.ch>
2021-01-17 14:03:55 +0100

    Timline to open generalDoors

--- js/animate.js
+++ js/animate.js
@@ -1,41 +1,53 @@
 $(document).ready(function () {
-    console.log("ready!");
 
 
-    var controller = new ScrollMagic.Controller();
-
-    // General Tween
-    var upper = TweenMax.to("#upperGeneral", 2, {
-        top: "-50%",
-    });
-
-    var lower = TweenMax.to("#lowerGeneral", 2, {
-        top: "+50%",
-    });
+    var controller = new ScrollMagic.Controller(); //
 
 
-    // Upper Scene
-    var upperScene = new ScrollMagic.Scene({
+    var openDoors = new TimelineMax()
+        .add([
+				TweenMax.to("#upperGeneral", 2, {
+                top: "-30%",
+                ease: "power3.out",
+
+
+            }),
+				TweenMax.to("#lowerGeneral", 2, {
+                bottom: "-30%",
+                ease: "power3.out"
+
+            })
+			]);
+
+
+    // open Scene
+    var openScene = new ScrollMagic.Scene({
             triggerElement: "#general",
-            duration: 400,
+            duration: 300,
         })
-        .setTween(upper)
+        .setTween(openDoors)
         .addIndicators({
-            name: "tween css class"
+            name: "openDoors"
         }) // add indicators (requires plugin)
+        .triggerHook(0.2)
+        .addTo(controller);
+
+    //General Animation
+
+    var generalAnimation = new ScrollMagic.Scene({
+            triggerElement: "#general",
+            duration: 700,
+        })
         .setPin("#general")
+        .addIndicators({
+            name: "generalAnimation"
+        }) // add indicators (requires plugin)
         .triggerHook(0)
         .addTo(controller);
 
-    // Lower Scene
-    var lowerScene = new ScrollMagic.Scene({
-            triggerElement: "#general",
-            duration: 400,
-        })
-        .setTween(lower)
-        .setPin("#general")
-        .triggerHook(0)
-        .addTo(controller);
+
+
+
 
 
 
