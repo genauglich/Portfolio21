c878056a410b6efc7f885a6c321f70c3747ed26f
Nico <nicostaehli@bluewin.ch>
2021-01-17 19:03:25 +0100

    First Animations

--- js/animate.js
+++ js/animate.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
 
 
+    /* _________SET CONTROLLER__________ */
+
+
     var controller = new ScrollMagic.Controller(); //
 
+    /* _________Gneral Open Doors__________ */
 
     var openDoors = new TimelineMax()
         .add([
 				TweenMax.to("#upperGeneral", 2, {
-                top: "-30%",
+                top: "-38%",
                 ease: "power3.out",
+                boxShadow: "0px 1px 40px 16px rgba(0,0,0,0.36)",
 
 
             }),
 				TweenMax.to("#lowerGeneral", 2, {
-                bottom: "-30%",
-                ease: "power3.out"
+                bottom: "-40%",
+                ease: "power3.out",
+                boxShadow: "0px 1px 40px 16px rgba(0,0,0,0.36)",
 
             })
 			]);
@@ -23,7 +29,7 @@ $(document).ready(function () {
     // open Scene
     var openScene = new ScrollMagic.Scene({
             triggerElement: "#general",
-            duration: 300,
+            duration: 500,
         })
         .setTween(openDoors)
         .addIndicators({
@@ -32,19 +38,61 @@ $(document).ready(function () {
         .triggerHook(0.2)
         .addTo(controller);
 
+
+    /* _________Gneral Main ANIMATION__________ */
+
+    // testDiv
+    var test = TweenMax.to("#testDiv", 2, {
+        opacity: "0.1",
+    })
+
     //General Animation
 
     var generalAnimation = new ScrollMagic.Scene({
             triggerElement: "#general",
-            duration: 700,
+            duration: 1000,
         })
         .setPin("#general")
+        .setTween(test)
         .addIndicators({
             name: "generalAnimation"
         }) // add indicators (requires plugin)
         .triggerHook(0)
         .addTo(controller);
 
+    /* _________Gneral close Doors (evtl oben einfügen)__________ */
+
+    //General close Dors
+
+    var closeDoors = new TimelineMax()
+        .add([
+				TweenMax.to("#upperGeneral", 2, {
+                top: "0",
+                ease: "power3.out",
+                boxShadow: "0",
+
+
+            }),
+				TweenMax.to("#lowerGeneral", 2, {
+                bottom: "0",
+                ease: "power3.out",
+                boxShadow: "0",
+
+            })
+			]);
+
+
+    // open Scene
+    var closeDoors = new ScrollMagic.Scene({
+            triggerElement: "#general",
+            duration: 300,
+        })
+        .setTween(openDoors)
+        .addIndicators({
+            name: "closeDoors"
+        }) // add indicators (requires plugin)
+        .triggerHook(0.5)
+        .addTo(controller);
 
 
 
