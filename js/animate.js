6712a421bef397858a886e66022973586adf0876
Nico <nicostaehli@bluewin.ch>
2021-01-21 23:20:00 +0100

    Update mit new Scene

--- js/animate.js
+++ js/animate.js
@@ -8,16 +8,16 @@ $(document).ready(function () {
 
     /* _________Gneral Open Doors__________ */
 
-    var openDoors = new TimelineMax()
+    var mainAnimation = new TimelineMax()
         .add([
-				TweenMax.to("#upperGeneral", 2, {
+				TweenMax.to("#upperGeneral", 7, {
                 top: "-38%",
                 ease: "power3.out",
                 boxShadow: "0px 1px 40px 16px rgba(0,0,0,0.36)",
 
 
             }),
-				TweenMax.to("#lowerGeneral", 2, {
+				TweenMax.to("#lowerGeneral", 3, {
                 bottom: "-40%",
                 ease: "power3.out",
                 boxShadow: "0px 1px 40px 16px rgba(0,0,0,0.36)",
@@ -25,73 +25,76 @@ $(document).ready(function () {
             })
 			]);
 
+    var msg = TweenMax.fromTo("#general h2", 1, {
+        top: 10,
+        left: 370,
+        rotation: 20
+    }, {
+        top: 100,
+        rotation: -20
+    });
 
+    //    mainAnimation.add(msg);
+    new ScrollMagic.Scene({
+            duration: 300,
+            offset: 200
+        }).setTween(msg)
+        .addTo(controller);
     // open Scene
     var openScene = new ScrollMagic.Scene({
             triggerElement: "#general",
-            duration: 100,
-        })
-        .setTween(openDoors)
-        .addIndicators({
-            name: "openDoors"
-        }) // add indicators (requires plugin)
-        .triggerHook(0.2)
-        .addTo(controller);
-
-
-    /* _________Gneral Main ANIMATION__________ */
-
-    var mainAnimation = new TimelineMax()
-
-    // testDiv
-    var test = TweenMax.staggerTo("#testDiv", 2, {
-        opacity: "0.1",
-        top: "300px",
-
-    });
-
-    var closeDoors = new TimelineMax()
-        .add([
-        				TweenMax.staggerTo("#upperGeneral", 2, {
-                top: "0",
-                ease: "power3.out",
-                boxShadow: "0",
-
-
-            }),
-        				TweenMax.staggerTo("#lowerGeneral", 2, {
-                bottom: "0",
-                ease: "power3.out",
-                boxShadow: "0",
-
-            })
-        			]);
-
-
-    mainAnimation.add(test).add(closeDoors);
-
-    //General Animation
-
-    var generalAnimation = new ScrollMagic.Scene({
-            triggerElement: "#general",
             duration: 1000,
         })
-        .setPin("#general")
         .setTween(mainAnimation)
+        .setPin("#general")
         .addIndicators({
-            name: "generalAnimation"
+            name: "mainAnimation"
         }) // add indicators (requires plugin)
         .triggerHook(0)
         .addTo(controller);
-
-    /*____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________*/
-
-    var controller_h = new ScrollMagic.Controller({
-        vertical: false
-    });
-
 
 
 
 
 });
+
+///* _________Gneral Main ANIMATION__________ */
+//
+//var mainAnimation = new TimelineMax()
+//
+//
+//});
+//
+//var closeDoors = new TimelineMax()
+//    .add([
+//        				TweenMax.staggerTo("#upperGeneral", 2, {
+//                top: "0",
+//                ease: "power3.out",
+//                boxShadow: "0",
+//
+//
+//            }),
+//        				TweenMax.staggerTo("#lowerGeneral", 2, {
+//                bottom: "0",
+//                ease: "power3.out",
+//                boxShadow: "0",
+//
+//            })
+//
+//
+//
+//mainAnimation.add(closeDoors);
+//
+////General Animation
+//
+//var generalAnimation = new ScrollMagic.Scene({
+//                    triggerElement: "#general",
+//                    duration: 1000,
+//                })
+//                .setPin("#general")
+//                .setTween(mainAnimation)
+//                .addIndicators({
+//                    name: "generalAnimation"
+//                }) // add indicators (requires plugin)
+//                .triggerHook(0)
+//                .addTo(controller);
